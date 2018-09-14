7a80419a15846cd20aa6df71bbdf3813c236d4a2
zuhxs@berkeley.edu <zhusongbo@hotmail.com>
2018-09-14 01:25:28 -0700

    Songbo's first commit

--- foobar.rb
+++ foobar.rb
@@ -1,6 +1,18 @@
 class Foobar
 
   def self.baz(a)
+  	a.map! {|i| i.to_i}
+  	a = a | []  # remove dump
+  	#print a
+  	result = 0
+  	a.each do |i|
+  		if i % 2 == 0 and i < 10 then
+  			result += 2
+  			result += i
+  			#puts result
+  		end
+  	end
+  	return result
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,17 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  return (a + b) ** 2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  return a.sort.map{|i| i + 1}
 
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  return first_name + " " + last_name
 
 end
 
@@ -49,4 +51,9 @@ def scrabble(word)
     z: 10,
   }
   # Q5 CODE HERE
+  result = 0
+  word.each_char do |i|
+    result += values[i.intern]
+  end
+  return result
 end
